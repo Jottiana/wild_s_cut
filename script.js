7a438b955ca648a9d9c11ba44fc7c35098e21099
JlCln <j.coulon.pro@gmail.com>
2024-10-03 16:55:43 +0200

    added recommendations + fixed margin on search bar

--- script.js
+++ script.js
@@ -4,55 +4,58 @@ import { getRandomInt } from "./common/index.js";
 
 const cardsContainer = document.querySelector(".movies-container-all");
 
-let watchlist = localStorage.getItem("watchlist");
+let watchlist = JSON.parse(localStorage.getItem("watchlist"));
+if (watchlist === undefined || watchlist === null) {
+  watchlist = [];
+}
 
 document.addEventListener("load", function () {
-	filterSelect(null);
+  filterSelect(null);
 });
 
 function emptyList() {
-	while (cardsContainer.lastElementChild) {
-		cardsContainer.removeChild(cardsContainer.lastElementChild);
-	}
+  while (cardsContainer.lastElementChild) {
+    cardsContainer.removeChild(cardsContainer.lastElementChild);
+  }
 }
 
 function showAll() {
-	showMoviesOfArray(movies);
+  showMoviesOfArray(movies);
 }
 
 function filterSelect(genre) {
-	emptyList();
-	const filteredMovies = movies.filter((movie) => {
-		if (Array.isArray(movie.type)) {
-			return movie.type.includes(genre) || genre === null;
-		} else {
-			return movie.type === genre || genre === null;
-		}
-	});
+  emptyList();
+  const filteredMovies = movies.filter((movie) => {
+    if (Array.isArray(movie.type)) {
+      return movie.type.includes(genre) || genre === null;
+    } else {
+      return movie.type === genre || genre === null;
+    }
+  });
 
-	showMoviesOfArray(filteredMovies);
+  showMoviesOfArray(filteredMovies);
 }
 
 function showMoviesOfArray(movieArray) {
-	emptyList();
-	movieArray.forEach((movie) => showMovie(movie));
+  emptyList();
+  movieArray.forEach((movie) => showMovie(movie));
 }
 
 function showMovie(movie) {
-	const card = createCard(movie);
-	cardsContainer.appendChild(card);
+  const card = createCard(movie);
+  cardsContainer.appendChild(card);
 }
 
 const buttons = document.querySelectorAll(".filter-btn");
 
 buttons.forEach((button) => {
-	button.addEventListener("click", () => {
-		if (button.dataset.filter === undefined) {
-			showAll();
-		} else {
-			filterSelect(button.dataset.filter);
-		}
-	});
+  button.addEventListener("click", () => {
+    if (button.dataset.filter === undefined) {
+      showAll();
+    } else {
+      filterSelect(button.dataset.filter);
+    }
+  });
 });
 
 document.addEventListener("DOMContentLoaded", showAll);
@@ -60,34 +63,34 @@ document.addEventListener("DOMContentLoaded", showAll);
 let search = document.querySelector(".search");
 let searchInput = document.querySelector("#search-input");
 search.onclick = function () {
-	document.querySelector(".search-box-div").classList.toggle("active");
+  document.querySelector(".search-box-div").classList.toggle("active");
 };
 
 searchInput.oninput = function () {
-	console.log(this.value);
+  console.log(this.value);
 
-	filterByText(this.value);
+  filterByText(this.value);
 };
 
 function filterByText(searchText) {
-	const loweredSearchText = searchText.toLowerCase();
-	emptyList();
+  const loweredSearchText = searchText.toLowerCase();
+  emptyList();
 
-	const filteredMovies = movies.filter(
-		(movie) =>
-			movie.name.toLowerCase().includes(loweredSearchText) ||
-			movie.desc.toLowerCase().includes(loweredSearchText),
-	);
-	showMoviesOfArray(filteredMovies);
+  const filteredMovies = movies.filter(
+    (movie) =>
+      movie.name.toLowerCase().includes(loweredSearchText) ||
+      movie.desc.toLowerCase().includes(loweredSearchText)
+  );
+  showMoviesOfArray(filteredMovies);
 }
 
 function showWatchlist() {
-	emptyList();
-	let watchlist = JSON.parse(localStorage.getItem("watchlist"));
-	let watchlistMovies = movies.filter((movie) =>
-		watchlist.includes(movie.name),
-	);
-	showMoviesOfArray(watchlistMovies);
+  emptyList();
+  let watchlist = JSON.parse(localStorage.getItem("watchlist"));
+  let watchlistMovies = movies.filter((movie) =>
+    watchlist.includes(movie.name)
+  );
+  showMoviesOfArray(watchlistMovies);
 }
 
 let watchlistBtn = document.getElementById("watchlist-show");
@@ -96,16 +99,16 @@ watchlistBtn.addEventListener("click", showWatchlist);
 
 // Surprise part
 function getMovieByIndex(index) {
-	return movies[index];
+  return movies[index];
 }
 
 function getRandomMovie() {
-	return getMovieByIndex(getRandomInt(movies.length));
+  return getMovieByIndex(getRandomInt(movies.length));
 }
 
 function showSurpriseMovie() {
-	emptyList();
-	showMovie(getRandomMovie());
+  emptyList();
+  showMovie(getRandomMovie());
 }
 
 const surprise = document.getElementById("surprise");
